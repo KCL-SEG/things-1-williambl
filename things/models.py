92dae2b1f52d75ca60e75749debe91a9fc8ec90c
williambl <william@williambl.com>
2022-10-14 17:46:09 +0100

    feat: add Thing model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Thing(models.Model):
+    name = models.TextField()
+    description = models.BigAutoField()
+    quantity = models.IntegerField()
