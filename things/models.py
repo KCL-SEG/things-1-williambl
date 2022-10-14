f8dd6e2bc5d6853b5bfa9e475f966249cf675e9a
williambl <william@williambl.com>
2022-10-14 18:03:46 +0100

    fix: description not being blankable

--- things/models.py
+++ things/models.py
@@ -5,5 +5,5 @@ from django.core import validators
 
 class Thing(models.Model):
     name = models.SlugField(unique=True,validators=[validators.MinLengthValidator(1), validators.MaxLengthValidator(30)])
-    description = models.TextField(validators=[validators.MaxLengthValidator(120)])
+    description = models.TextField(blank=True, validators=[validators.MaxLengthValidator(120)])
     quantity = models.IntegerField(validators=[validators.MinValueValidator(0), validators.MaxValueValidator(100)])
