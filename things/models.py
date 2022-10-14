3710473aba0af7d30d817ceee58731be2c5718b9
williambl <william@williambl.com>
2022-10-14 17:58:50 +0100

    feat: constraints on model fields

--- things/models.py
+++ things/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core import validators
 
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.TextField()
-    description = models.BigAutoField()
-    quantity = models.IntegerField()
+    name = models.SlugField(unique=True,validators=[validators.MinLengthValidator(1), validators.MaxLengthValidator(30)])
+    description = models.TextField(validators=[validators.MaxLengthValidator(120)])
+    quantity = models.IntegerField(validators=[validators.MinValueValidator(0), validators.MaxValueValidator(100)])
